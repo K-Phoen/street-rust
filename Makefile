9bcc816c228851b402ef88d95b0d2b0c499230ce
Kévin Gomez <kgomez@tea-ebook.com>
2017-05-09 16:48:55 +0200

    Require diesel

--- Makefile
+++ Makefile
@@ -3,3 +3,11 @@ build:
 
 serve:
 	cargo run
+
+db_start:
+	docker run --name street-rust-pg -e POSTGRES_PASSWORD=street_rust_password -e POSTGRES_USER=street_rust -p 5432:5432 -d postgres:9.6
+
+db_stop:
+	docker stop street-rust-pg
+
+.PHONY: db_start db_stop
--- src/controllers/murals.rs
+++ src/controllers/murals.rs
@@ -22,9 +22,15 @@ pub fn list(_ : &mut Request) -> IronResult<Response> {
         coord: Coordinates { lat: 5.55, long: 6.66 },
         title: String::from("Some awesome mural")
     };
+    let other_mural = Mural {
+        coord: Coordinates { lat: 5.55, long: 6.66 },
+        title: String::from("Some awesome mural")
+    };
+
+    let murals = vec![mural, other_mural];
 
     // Convert the Point to a JSON string.
-    let serialized = serde_json::to_string(&mural).unwrap();
+    let serialized = serde_json::to_string(&murals).unwrap();
 
     Ok(Response::with((ContentType::json().0, status::Ok, serialized)))
 }
